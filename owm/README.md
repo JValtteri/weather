7366234dbaae716cc5ae3ca1d9fbe4235cbc3514
JValtteri <46611993+JValtteri@users.noreply.github.com>
2025-02-28 18:32:28 +0200

    Updated documentation

--- owm/README.md
+++ owm/README.md
@@ -2,4 +2,38 @@
 
 Package for interfacing with openweathermap.org basic APIs
 
+### `owm.Config()`
+```go
+func Config(key, units, countryCode string)
+```
+Config initializes the API. Run Config() before using the API.
 
+### `owm.Forecast()`
+```go
+func Forecast(lat float32, lon float32) WeatherRange
+```
+Forecast fetches forecast data by coordinates and returns a WeatherRange object.
+Use Coord to find the coordinates of a cities.
+
+### `owm.Coord()`
+```go
+func Coord(name string) (float32, float32)
+```
+Coord returns the coordinates for the given city
+
+### `owm.Icon()`
+```go
+func Icon(id string) []byte
+```
+Icon returns the icon PNG image in byte form.
+
+The `id` is a icon code that is found inside the forecast object
+
+### Response objects
+
+### [`input_types.go`](input_types.go)
+
+Some fields may not be implemented
+
+---
+For an example implementation, see [JValtteri/ll-weather-server](https://github.com/JValtteri/ll-weather-server)
--- owm/openweatherApi.go
+++ owm/openweatherApi.go
@@ -1,5 +1,10 @@
 package owm
 
+/* OpenWeatherMap API library OWM
+ * Library for fetching forecasts using the
+ * free OpenWeatherMap API
+ */
+
 import (
     "net/http"
     "encoding/json"
@@ -13,6 +18,9 @@ const FORECAST_URL string = "https://api.openweathermap.org/data/2.5/forecast?la
 const CITY_URL string = "https://api.openweathermap.org/geo/1.0/direct?q={CITY_NAME},{COUNTRY_CODE}&limit={LIMIT}&appid={API_KEY}"
 const ICON_URL = "https://openweathermap.org/img/wn/{ICON}@2x.png"
 
+/* Structure to store the configuration
+ * Populate with owm.Config(key, units, countryCode string)
+ */
 type Api_config struct {
     API_KEY    string
     UNITS      string // metric, imperial, standard (Kelvin)
@@ -22,6 +30,8 @@ type Api_config struct {
 
 var API_CONFIG Api_config
 
+/* Run Config() first, to initialize the API
+ */
 func Config(key, units, countryCode string) {
     API_CONFIG.API_KEY    = key
     API_CONFIG.UNITS      = units
@@ -29,6 +39,8 @@ func Config(key, units, countryCode string) {
     API_CONFIG.CITY_LIMIT = "1"
 }
 
+/* Forecast fetches forecast data by coordinates and returns a WeatherRange object
+ */
 func Forecast(lat float32, lon float32) WeatherRange {
     var weather_obj WeatherRange
     var requestURL string = weatherURL(lat, lon, API_CONFIG.UNITS)
@@ -40,6 +52,8 @@ func Forecast(lat float32, lon float32) WeatherRange {
     return weather_obj
 }
 
+/* Coord returns the coordinates for the given city
+ */
 func Coord(name string) (float32, float32) {
     var city_obj InCity
     var requestURL string = cityURL(name, API_CONFIG.COUNTRY, API_CONFIG.CITY_LIMIT)
@@ -52,6 +66,8 @@ func Coord(name string) (float32, float32) {
     return lat, lon
 }
 
+/* Icon returns the icon image in byte form.
+ */
 func Icon(id string) []byte {
     var requestURL string = makeIconURL(id)
     var raw_icon []byte   = request(requestURL)
